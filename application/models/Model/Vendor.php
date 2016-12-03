9acd73d73f54482ed2f4350d64a7d82efb797b43
louisrui <louis.goomail@gmail.com>
2016-12-03 20:49:37 +0700

    Add files via upload

--- application/models/Model/Vendor.php
+++ application/models/Model/Vendor.php
@@ -1,34 +1,28 @@
 <?
 class Asset_model extends CI_Model {
-        public $vendor_id;
-       
+        public $Vendor_id;
+		public $data;
+		public $condition;
         public function __construct()
         {
                 // Call the CI_Model constructor
                 parent::__construct();
                 //$this->load->database();
         }
-        public function get_vendor()
-        {
-                $query = $this->db->get('AssetMain');
-                return $query->result();
-        }
-        public function get_vendor_by_id($vendor_id)
-        {
-                if($asset_id != FALSE) {
-                    $this->db->where('Vendor',$asset_id);
-                    $query = $this->db->get('Vendor');
-                    return $query->result();
-                  }
-                else 
-					{
-						return FALSE;
-					}
-        }
+		public function get($condition = NULL)
+		{
+			if(!empty($condition['VendorID']))
+			{
+				if(is_array($condition['VendorID']))
+					$this->db->where_in($condition['VendorID']);
+				else
+					$this->db->where($condition['VendorID']);
+			}	
+			return $this->db->get('Vendor');
+		}
         public function insert_asset($data)             //array of data (1 row)
         {
-                
-                $this->db->insert('Vendor',$data);
+			$this->db->insert('Vendor',$data);
         }
         public function delete_vendor_by_id($Vendor_id) 
         {
@@ -36,8 +30,8 @@ class Asset_model extends CI_Model {
         }
         public function edit_vendor_by_id($Vendor_id,$data)
         {
-                
-                $this->db->update('VendorMain', $data, array('VendorID' => $Vendor_id))
+			$this->db->update('VendorMain', $data, array('VendorID' => $Vendor_id))
         }
+
 }
 ?>
