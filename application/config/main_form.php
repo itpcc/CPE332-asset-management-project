35093388cf515ba92f3ace825057b66534eb0e81
Rachasak Ragkamnerd <id513128@gmail.com>
2016-12-04 04:58:19 +0700

    Add employee, role (Unfinish) before sleep

--- application/config/main_form.php
+++ application/config/main_form.php
@@ -197,3 +197,153 @@
 			array('CompanyEmail')
 		)
 	);
+
+	$config['section_detail']['employee'] = array(
+		'name'	=> 'Employee',
+		'icon'	=> 'work',
+		'slug'	=> 'employee',
+		'color'	=> 'blue',
+		'modal'	=> array(
+			'header'	=> 'จัดการพนักงาน'
+		),
+		'add'	=> array(
+			'icon'	=> 'add'
+		),
+		'fields'	=> array(
+			'EmployeeID'	=> array(
+				'input'	=> array(
+					'type'	=> 'hidden'
+				),
+				'name'	=> '#',
+				'slug'	=> 'id'
+			),
+			'FirstName'	=> array(
+				'input'	=> array(
+					'type'	=> 'text',
+					'required'	=> true
+				),
+				'name'	=> 'First Name',
+				'slug'	=> 'first_name',
+				'icon'	=> 'account_circle'
+			),
+			'LastName'	=> array(
+				'input'	=> array(
+					'type'	=> 'text',
+					'required'	=> true
+				),
+				'name'	=> 'Last Name',
+				'slug'	=> 'last_name'
+			),
+			'Address'	=> array(
+				'input'	=> array(
+					'type'	=> 'textarea',
+					'required'	=> true,
+					'length'	=> 100
+				),
+				'name'	=> 'Address',
+				'slug'	=> 'address',
+				'icon'	=> 'home'
+			),
+			'PhoneNO'	=> array(
+				'input'	=> array(
+					'type'	=> 'tel',
+					'required'	=> true
+				),
+				'name'	=> 'Phone',
+				'slug'	=> 'phone',
+				'icon'	=> 'phone'
+			),
+			'Email'	=> array(
+				'input'	=> array(
+					'type'	=> 'email',
+					'required'	=> true
+				),
+				'name'	=> 'Email',
+				'slug'	=> 'email',
+				'icon'	=> 'email',
+				'validation'	=> 'valid_email'
+			),
+			'RoleID'	=> array(
+				'input'	=> array(
+					'type'	=> 'select',
+					'required'	=> true,
+					'module'	=> 'roleandpermission',
+					'option_text'	=> 'RoleName',
+					'option_id'	=> 'RoleID'
+				),
+				'name'	=> 'Role',
+				'slug'	=> 'role'
+			),
+		),
+		'id_key'	=> 'EmployeeID',
+		'url'	=> array(
+			'list'	=> base_url('index.php/employee'),
+			'add'	=> base_url('index.php/employee/add'),
+			'edit'	=> base_url('index.php/employee/edit')
+		),
+		'table'	=> array(
+			'EmployeeID',
+			'FirstName',
+			'LastName',
+			'Address',
+			'PhoneNO',
+			'Email',
+			array(
+				'localField'	=> 'RoleID',
+				'module'	=> 'roleandpermission',
+				'targetField'	=> 'RoleName'
+			),
+		),
+		'form_lines'	=> array(
+			array('EmployeeID'),
+			array('FirstName', 'LastName'),
+			array('Address'),
+			array('PhoneNO', 'Email'),
+			array('RoleID')
+		)
+	);
+
+	$config['section_detail']['roleandpermission'] = array(
+		'name'	=> 'Role&Permision',
+		'icon'	=> 'vpn_key',
+		'slug'	=> 'role-permision',
+		'color'	=> 'grey',
+		'modal'	=> array(
+			'header'	=> 'จัดการหน้าที่'
+		),
+		'add'	=> array(
+			'icon'	=> 'vpn_key add'
+		),
+		'fields'	=> array(
+			'RoleID'	=> array(
+				'input'	=> array(
+					'type'	=> 'hidden'
+				),
+				'name'	=> '#',
+				'slug'	=> 'id'
+			),
+			'RoleName'	=> array(
+				'input'	=> array(
+					'type'	=> 'text',
+					'required'	=> true
+				),
+				'name'	=> 'Role Name',
+				'slug'	=> 'role_name',
+				'icon'	=> 'label_outline'
+			)
+		),
+		'id_key'	=> 'RoleID',
+		'url'	=> array(
+			'list'	=> base_url('index.php/roleandpermission'),
+			'add'	=> base_url('index.php/roleandpermission/add'),
+			'edit'	=> base_url('index.php/roleandpermission/edit')
+		),
+		'table'	=> array(
+			'RoleID',
+			'RoleName'
+		),
+		'form_lines'	=> array(
+			array('RoleID'),
+			array('RoleName')
+		)
+	);
--- assets/js/init.js
+++ assets/js/init.js
@@ -6,3 +6,336 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function _bindAjaxLinkElement(){
+	$("a:not([data-bind-ajax], a[href^='#'])").each(function(){
+		if(
+			this.hostname === BASE_URL_ELEMENT.hostname && 	//check is in the same domain
+			this.pathname.indexOf(BASE_URL_ELEMENT.pathname) === 0 //and in the same directory (localhost are placed in subdir)
+		){
+			$(this).attr("data-bind-ajax", "true").click(function(e){
+				var currURL = this.href;
+				e.preventDefault();
+				if(!currURL.match(/[?&]api=/)){
+					currURL = BASE_URL+'?api='+encodeURIComponent(this.pathname.substring(BASE_URL_ELEMENT.pathname.length));
+					if(this.search)
+						currURL += '&'+this.search.substring(1); // Strip ? and append into exist query string
+				}
+
+				console.log("going to URL: ", currURL);
+				if(History.getState().cleanUrl !== currURL){
+					History.pushState(null, document.title + ' | '+ ($(this).attr("title") || $(this).text()), currURL);
+				}else{
+					//if state not change
+					console.log("Manual: ", currURL);
+					gotoPage(currURL); //go manually
+				}
+				// cancel the normal action of the hyperlink
+				return false;
+			});
+		}else{
+			$(this).attr("data-bind-ajax", "false");
+		}
+	});
+}
+
+function scrollTo(selector){
+	$('html, body').animate({
+		scrollTop: $(selector).offset().top
+	}, 500);
+}
+
+function gotoPage(url){
+	var urlObj = document.createElement("a");
+	var urlQueryObj, urlPath;
+	urlObj.href = url;
+	if(!urlObj.search)
+		return false;
+
+	urlQueryObj = $.deparam(urlObj.search.substring(1));
+
+	if(urlQueryObj.api){
+		urlPath = $.grep(urlQueryObj.api.split('/'), function(elem){ return elem.length > 0; });
+		console.log(urlPath);
+		if(!jQuery.isEmptyObject(SECTION_FORM_CONFIG[urlPath[0]])){
+			scrollTo('#'+urlPath[0]+'-section');
+			if(urlPath[1] === 'add'){
+				openForm(urlPath[0]);
+			}else if(urlPath[1] === 'edit' && typeof urlPath[2] !== "undefined"){
+				dataResult = getModuleData(urlPath[0], urlPath[2]);
+				if(dataResult === false){
+					Materialize.toast("Cannot get data from "+urlPath[0]+':'+urlPath[2], 2000);
+				}else if(dataResult === true){
+					//@TODO loading
+					$(window).one('data-'+urlPath[0]+'-'+urlPath[2]+'-sent', function(){
+						dataResult = getModuleData(urlPath[0], urlPath[2]);
+						openForm(urlPath[0], dataResult);
+					});
+				}else{
+					openForm(urlPath[0], dataResult);
+				}
+			}
+		}else{
+			jQuery.noop();
+		}
+	}
+}
+
+function replaceModuleDataPlaceholder(sectionID, elemID){
+	$(".app-module-data-placeholder[data-module-name='"+sectionID+"'][data-module-id='"+elemID+"']").each(function(){
+		var fieldName = $(this).attr("data-field-name");
+		if(
+			!jQuery.isEmptyObject(window.moduleData[sectionID][elemID]) && 
+			typeof window.moduleData[sectionID][elemID][fieldName] !== "undefined"
+		){
+			$(this).html(window.moduleData[sectionID][elemID][fieldName]);
+			$(this).removeClass('app-module-data-placeholder');
+		}
+	});
+}
+
+function getModuleData(sectionID, elemID){
+	try{
+		if(!jQuery.isEmptyObject(window.moduleData[sectionID])){
+			if(!jQuery.isEmptyObject(window.moduleData[sectionID][elemID])){
+				replaceModuleDataPlaceholder(sectionID, elemID);
+				return window.moduleData[sectionID][elemID];
+			}
+		}else{
+			window.moduleData[sectionID] = {};
+		}
+
+		if(!jQuery.isEmptyObject(SECTION_FORM_CONFIG[sectionID]['url']['list'])){
+			$.getJSON(SECTION_FORM_CONFIG[sectionID]['url']['list'], { id: elemID }, function(ajaxData){
+				window.moduleData[sectionID][elemID] = ajaxData.data.pop();
+				replaceModuleDataPlaceholder(sectionID, elemID);
+				$(window).trigger('data-'+sectionID+'-'+elemID+'-sent', [elemID]);
+			});
+			return true;
+		}
+	}catch(e){
+		console.error('Error while getting module data: ', e);
+		return false;
+	}
+	return false;				
+}
+
+function openForm(formID, formData){
+	if(jQuery.isEmptyObject(SECTION_FORM_CONFIG[formID]))
+		return;
+
+	console.log("FormData: ", formData);
+
+	$('#app-modal-'+formID+' :input').removeClass("valid invalid");
+	if(!jQuery.isEmptyObject(formData)){
+		$.each(formData, function(key, val){
+			$("#app-modal-"+formID+"-"+key).val(val);
+		});
+	}else{
+		$('#app-modal-'+formID+' :input').val('');
+	}
+
+	jQuery.each(SECTION_FORM_CONFIG[formID].fields, function(fieldName, fieldProperties){
+		if(fieldProperties.input.type === "select2"){
+			$('#app-modal-'+formID+'-'+fieldProperties.slug).select2({
+				ajax: {
+					url: fieldProperties.input.ajax,
+					dataType: 'json',
+					processResults: function (ajaxData) {
+						//console.log(data);
+						return {
+							results: $.map(ajaxData.data, function (item) {
+								console.log(item);
+								return {
+									text: item[fieldProperties.input.ajax_text],
+									slug: item[fieldProperties.input.ajax_id],
+									id: item[fieldProperties.input.ajax_id]
+								};
+							})
+						};
+					}
+				},
+				minimumInputLength: 2,
+				width: '100%'
+			});
+			if(!jQuery.isEmptyObject(formData) && formData[fieldName]){
+				$.ajax({
+					cache: true,
+					dataType: "json",
+					url: fieldProperties.input.ajax_query_id+formData[fieldName],
+					success: function(ajaxData){
+						$('#app-modal-'+formID+'-'+fieldProperties.slug).append('<option value="'+ajaxData.data[0][fieldProperties.input.ajax_id]+'">'+ajaxData.data[0][fieldProperties.input.ajax_text]+'</option>').val(ajaxData.data[0][fieldProperties.input.ajax_id]).trigger('change');
+					}
+				});
+			}else{
+				$('#app-modal-'+formID+'-'+fieldProperties.slug).val('').trigger('change');
+			}
+		}else if(fieldProperties.input.type === "select"){
+			//@TODO: Do Select
+		}else{
+			if(typeof fieldProperties.input.length === "number"){
+				$('#app-modal-'+formID+'-'+fieldProperties.slug).trigger('autoresize').characterCounter();
+			}
+
+			if(!jQuery.isEmptyObject(formData) && !jQuery.isEmptyObject(formData[fieldName])){
+				$('#app-modal-'+formID+'-'+fieldProperties.slug).val(formData[fieldName]).trigger('change');
+			}
+
+		}
+	});
+
+	$("#app-modal-"+formID).modal('open');
+
+	if(jQuery.isEmptyObject(window.alreadyBindEvent['form']))
+		window.alreadyBindEvent['form'] = {};
+
+	if(!window.alreadyBindEvent['form'][formID]){
+		console.log("Bind :", window.alreadyBindEvent['form']);
+		$('#app-modal-'+formID+' .app-modal-submit').click(function(e){
+			var error = {};
+			e.preventDefault();
+			console.log('#app-modal-'+formID+' [required]');
+			$('#app-modal-'+formID+' :input').each(function(){
+				$(this).removeClass("invalid");
+
+				if(!this.validity.valid){
+					error[$(this).attr("id") || $(this).attr("name")] = ($("label[for='"+$(this).attr("id")+"']").text() || $(this).attr("name")).toString()+' : '+this.validationMessage;
+				}
+			});
+
+			if(!jQuery.isEmptyObject(error)){
+				Materialize.toast(Object.values(error).join('<br>'), 4000); // 4000 is the duration of the toast
+				$.each(error, function(key, name){
+					$('#'+key+', #app-modal-'+formID+' input[name="'+key+'"]').addClass("invalid");
+				});
+			}else{
+				console.log("Submit Data: ", $('#app-modal-'+formID+' .app-modal-form').serialize());
+				$.ajax({
+					dataType: 'json',
+					method: 'POST',
+					data: $('#app-modal-'+formID+' .app-modal-form').serialize(),
+					error: function(jqXHR, textStatus, errorThrown){
+						Materialize.toast('<span class="text-red">Error while submit data ('+textStatus+'):'+errorThrown+'</span>', 4000); // 4000 is the duration of the toast
+					},
+					success: function(data){
+						if(data.error){
+							Materialize.toast('<span class="red-text">Error: '+((typeof data.error === "string")?data.error:JSON.stringify(data.error))+'</span>', 4000); // 4000 is the duration of the toast
+						}else{
+							Materialize.toast('<span class="green-text">Success!</span>', 2000); // 4000 is the duration of the toast
+							$("#app-modal-"+formID).modal('close');
+							generateTable(formID);
+						}
+					},
+					url:( 
+						jQuery.isEmptyObject($('#app-modal-'+formID+'-'+SECTION_FORM_CONFIG[formID].fields[SECTION_FORM_CONFIG[formID].id_key].slug).val())?
+						SECTION_FORM_CONFIG[formID].url.add:
+						SECTION_FORM_CONFIG[formID].url.edit
+					),
+
+				});
+			}
+		});
+		window.alreadyBindEvent['form'][formID] = true;
+	}
+}
+
+function generateTable(tableID){
+	if(jQuery.isEmptyObject(SECTION_FORM_CONFIG[tableID]))
+		return;
+	
+
+	if(!$('#'+tableID+'-section-list').attr('data-DataTable')){
+		var columnList = [], columnCustomRender = [{
+			"targets": -1,
+			"render": function ( data, type, row ) {
+				//console.log('data, type, row :', data, type, row);
+				return '<a href="'+tableID+'/edit/'+data[SECTION_FORM_CONFIG[tableID].id_key]+'" class="app-'+tableID+'-edit waves-effect waves-light btn">Edit!</a>';
+			}
+		}];
+		if(typeof window.columnGetDataList === "undefined")  window.columnGetDataList = {};
+		window.columnGetDataList[tableID] = [];
+		jQuery.each(SECTION_FORM_CONFIG[tableID].table, function(i, fieldProperties){
+			if(typeof fieldProperties === 'string'){
+				columnList.push({ data: fieldProperties });
+			}else{
+				columnList.push({ data: fieldProperties.localField });
+				columnCustomRender.push({
+					"targets": i,
+					"render": function ( data, type, row ) {
+						window.columnGetDataList[tableID].push([fieldProperties.module, row[fieldProperties.localField]]);
+						return '<span class="app-module-data-placeholder" data-module-name="'+fieldProperties.module+'" data-module-id="'+row[fieldProperties.localField]+'" data-field-name="'+fieldProperties.targetField+'">'+row[fieldProperties.localField]+'</span>';
+					}
+				});
+			}
+		});
+		columnList.push({ data: null });
+
+		console.log("columnList: ", columnList);
+		console.log("columnCustomRender: ", columnCustomRender);
+		window.dataTableObj[tableID] = $('#'+tableID+'-section-list').DataTable( {
+				responsive: true,
+				dom: '<<"row"<"col m8"l><"#'+tableID+'-section-list-searchbox.input-field col m4 right">><t>ip>',
+				ajax: SECTION_FORM_CONFIG[tableID].url.list,
+				columns: columnList,
+				"columnDefs": columnCustomRender,
+				"fnDrawCallback": function(){
+					_bindAjaxLinkElement();
+				}
+		} );
+		$('#'+tableID+'-section-list').attr('data-DataTable', 'true');
+		$('#'+tableID+'-section-list-searchbox').html('<input value="" id="'+tableID+'-section-filter-field" class="white-text" type="text"><label for="'+tableID+'-section-filter-field" class="white-text">Search:</label>');
+		$('#'+tableID+'-section-filter-field').keyup(function(){
+			$('#'+tableID+'-section-list').dataTable().search($(this).val()).draw() ;
+		});
+		window.dataTableObj[tableID].on('xhr', function(){
+			var ajaxJson = window.dataTableObj[tableID].ajax.json();
+			if(typeof window.moduleData[tableID] === 'undefined')
+				window.moduleData[tableID] = {};
+			$.each(ajaxJson.data, function(i, JSONRowData){
+				window.moduleData[tableID][JSONRowData.VendorID] = JSONRowData;
+			});
+
+			console.log('Table AJAX data: ', ajaxJson);
+		});
+		window.dataTableObj[tableID].on('draw.dt', function(){
+			if(!jQuery.isEmptyObject(window.columnGetDataList[tableID])){
+				jQuery.each(window.columnGetDataList[tableID], function(i, eachProperties){
+					window.columnGetDataList[tableID].pop(i);
+					if(!jQuery.isEmptyObject(eachProperties))
+						getModuleData(eachProperties[0], eachProperties[1]);
+				});
+			}
+		});
+	}else{
+		window.dataTableObj[tableID].ajax.reload();
+	}
+	Materialize.updateTextFields();
+}
+
+$(document).ready(function(){
+	window.dataTableObj = {};
+	window.moduleData = {};
+	window.alreadyBindEvent = {};
+	_bindAjaxLinkElement();
+	$('.modal').modal();
+	gotoPage(document.location);
+	$('.pushpin-nav').each(function() {
+		var $this = $(this);
+		var $target = $('#' + $(this).attr('data-target'));
+		$this.pushpin({
+			top: $target.offset().top,
+			bottom: $target.offset().top + $target.outerHeight() - $this.height()
+		});
+	});
+	jQuery.each(SECTION_FORM_CONFIG, function(sectionName, sectionProp){
+		generateTable(sectionName);
+	})
+});
+(function(window,undefined){
+	// Bind to StateChange Event
+	History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+		var State = History.getState(); // Note: We are using History.getState() instead of event.state
+		gotoPage(State.cleanUrl);
+		return true;
+	});
+})(window);
